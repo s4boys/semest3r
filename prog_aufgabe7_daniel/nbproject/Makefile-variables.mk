626582c30a23733fc5d8df4c0397a4be68802b00
DaniHi <34064281+DaniHi@users.noreply.github.com>
2018-04-27 14:05:58 +0200

    Add files via upload
    Prog3 Aufgabe 7

---
+++ prog_aufgabe7_daniel/nbproject/Makefile-variables.mk
@@ -0,0 +1,51 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug_WIN32 configuration
+CND_PLATFORM_Debug_WIN32=GNU-Linux
+CND_ARTIFACT_DIR_Debug_WIN32=dist/Debug_WIN32/GNU-Linux
+CND_ARTIFACT_NAME_Debug_WIN32=aufgabe_7
+CND_ARTIFACT_PATH_Debug_WIN32=dist/Debug_WIN32/GNU-Linux/aufgabe_7
+CND_PACKAGE_DIR_Debug_WIN32=dist/Debug_WIN32/GNU-Linux/package
+CND_PACKAGE_NAME_Debug_WIN32=aufgabe7.tar
+CND_PACKAGE_PATH_Debug_WIN32=dist/Debug_WIN32/GNU-Linux/package/aufgabe7.tar
+# Release_WIN32 configuration
+CND_PLATFORM_Release_WIN32=Cygwin-Windows
+CND_ARTIFACT_DIR_Release_WIN32=dist/Release_WIN32/Cygwin-Windows
+CND_ARTIFACT_NAME_Release_WIN32=aufgabe_7
+CND_ARTIFACT_PATH_Release_WIN32=dist/Release_WIN32/Cygwin-Windows/aufgabe_7
+CND_PACKAGE_DIR_Release_WIN32=dist/Release_WIN32/Cygwin-Windows/package
+CND_PACKAGE_NAME_Release_WIN32=aufgabe7.tar
+CND_PACKAGE_PATH_Release_WIN32=dist/Release_WIN32/Cygwin-Windows/package/aufgabe7.tar
+# Debug_LINUX configuration
+CND_PLATFORM_Debug_LINUX=GNU-Linux
+CND_ARTIFACT_DIR_Debug_LINUX=dist/Debug_LINUX/GNU-Linux
+CND_ARTIFACT_NAME_Debug_LINUX=aufgabe_7
+CND_ARTIFACT_PATH_Debug_LINUX=dist/Debug_LINUX/GNU-Linux/aufgabe_7
+CND_PACKAGE_DIR_Debug_LINUX=dist/Debug_LINUX/GNU-Linux/package
+CND_PACKAGE_NAME_Debug_LINUX=aufgabe7.tar
+CND_PACKAGE_PATH_Debug_LINUX=dist/Debug_LINUX/GNU-Linux/package/aufgabe7.tar
+# Release_LINUX configuration
+CND_PLATFORM_Release_LINUX=GNU-Linux
+CND_ARTIFACT_DIR_Release_LINUX=dist/Release_LINUX/GNU-Linux
+CND_ARTIFACT_NAME_Release_LINUX=aufgabe_7
+CND_ARTIFACT_PATH_Release_LINUX=dist/Release_LINUX/GNU-Linux/aufgabe_7
+CND_PACKAGE_DIR_Release_LINUX=dist/Release_LINUX/GNU-Linux/package
+CND_PACKAGE_NAME_Release_LINUX=aufgabe7.tar
+CND_PACKAGE_PATH_Release_LINUX=dist/Release_LINUX/GNU-Linux/package/aufgabe7.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
