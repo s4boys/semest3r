c36a1a8a2cb40b88e4dde7c8e6695333af1fe048
Hannes <hjd-97@web.de>
2018-05-18 11:45:47 +0200

    Grundstein

---
+++ prog_aufgabe9_hannes/foo.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#ifdef _WIN32
+#include <windows.h>
+const char *conf="WINDOWS";
+#endif
+#ifdef linux
+#define _GNU_SOURCE
+#include <dlfcn.h>
+const char *conf="LINUX";
+#else
+#error unknown configuration
+#endif 
+
+#if defined(WINDOWS)
+__declspec(dllexport)
+#endif
+void foo(const char *msg) {
+    printf("In foo, configuration %s, from %s\n", conf, msg);
+}
