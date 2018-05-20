9aaa8274e7d8fa0cc002ea5b23f7177aad750620
Unknown <71scfe1bif@hft-stuttgart.de>
2018-05-20 15:25:55 +0200

    added dynamic library creation

--- prog_aufgabe9_felix/main.c
+++ prog_aufgabe9_felix/main.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 #include "foo.c"
+/*
+#ifdef _WIN32
+#include "shared.dll"
+#else
+#include "shared.so"
+#endif
+*/
 extern void foo(const char* msg);
 
 const char* module= "main.c";
+
 
 int main(int argc, char** argv) {
     foo(module);
--- prog_aufgabe9_felix/shared.c
+++ prog_aufgabe9_felix/shared.c
@@ -1,6 +1,11 @@
 
+#include "foo.c"
+extern void foo(const char* msg);
+
 const char* module= "shared.c";
+
+__attribute__((constructor))
 void init() 
 {
-//__attribute__((constructor));
+    foo(module);
 }
